3876443a1dbefa3d54b5a54275245555a02afea7
Parker Brown <sparkermann@gmail.com>
2021-01-25 17:52:42 -0600

    Added additional Algos + folders

---
+++ HackerRank Algos/WarmUp Challenges/Sales_by_Match_1-25-2021.js
@@ -0,0 +1,61 @@
+// There is a large pile of socks that must be paired by color. Given an array of integers representing the color of each sock, 
+// determine how many pairs of socks with matching colors there are.
+
+// There is one pair of color  and one of color . There are three odd socks left, one of each color. The number of pairs is .
+
+// Function Description
+
+// int n: the number of socks in the pile
+// int ar[n]: the colors of each sock
+// Returns
+
+// int: the number of pairs
+// Input Format
+
+// The first line contains an integer , the number of socks represented in .
+// The second line contains  space-separated integers, , the colors of the socks in the pile.
+
+// STDIN                       Function
+// -----                       --------
+// 9                           n = 9
+// 10 20 20 10 10 30 50 10 20  ar = [10, 20, 20, 10, 10, 30, 50, 10, 20]
+// Sample Output
+
+// 3
+// Explanation
+
+// There are three pairs of socks.
+
+console.log(Math.floor(0.5));
+
+function sockMerchant(arr) {
+  var pairs = 0;
+  var numOfMatching = 0;
+  var matched = [];
+  for (i = 0; i < arr.length - 1; i++) {
+    if (matched.length > 0) {
+      for (m = 0; m < matched.length - 1; m++) {
+        if (arr[i] == matched[m]) {
+          //skip inner forloop and increment i
+          continue;
+        }
+      }
+    }
+    for (j = 0; j <= arr.length - 1; j++) {
+      if (arr[i] == arr[j]) {
+        console.log('outer for loop', arr[i]);
+        console.log('inner for loop:', arr[j]);
+        numOfMatching++;
+        console.log(numOfMatching);
+      }
+    }
+    matched.push(arr[i]);
+    var numOfMatched = numOfMatching / 2;
+    Math.floor(numOfMatched);
+    console.log('Matched:', numOfMatched);
+    pairs += numOfMatched;
+    numOfMatching = 0;
+  }
+  return(pairs);
+
+}
